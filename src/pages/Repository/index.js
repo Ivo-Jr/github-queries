e01ef30233a3c298e7c38edcdc9961e5caf0569b
IvoJr <maciel.jr95@gmail.com>
2021-03-27 14:55:12 -0300

    loading API data

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+export default class Repository extends Component {
+  async componentDidMount() {
+    const { match } = this.props;
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`),
+    ]);
+  }
+
+  render() {
+    return <h1>Repository:</h1>;
+  }
 }
